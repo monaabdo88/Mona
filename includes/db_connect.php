6b279750b4565f321b0bab3d16d13b8e9d795b9f
monaabdo88 <monaabdo88@gmail.com>
2017-01-28 14:53:31 +0200

    Update db_connect.php

--- includes/db_connect.php
+++ includes/db_connect.php
@@ -2,7 +2,7 @@
 $host_name = "localhost";
 $db_name = "weza";
 $server_name = "root";
-$server_pass="12122005";
+$server_pass="";
 $conn = mysqli_connect($host_name,$server_name,$server_pass,$db_name);
 if(mysqli_connect_errno()){
     die("Failed To Connect To The Database ".mysqli_connect_error());
